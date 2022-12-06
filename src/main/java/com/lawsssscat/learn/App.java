225e1639b996d4537809a52331744defedaec315
骆言 <1191693505@qq.com>
2022-12-06 13:14:18 +0800

    init

---
+++ src/main/java/com/lawsssscat/learn/App.java
@@ -0,0 +1,13 @@
+package com.lawsssscat.learn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
